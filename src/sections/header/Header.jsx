86f780c552e2325dd7779ec08eeab8719faf057d
Andres Bolivar <dre.bolivar@gmail.com>
2023-01-24 16:04:40 -0500

    now with less red cup

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import HeaderImage from '../../assets/header.jpg'
+import HeaderImage from '../../assets/header1.jpg'
 import data from './data'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
